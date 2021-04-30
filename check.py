afc8bc711f281d21682b2e44d77232bcdb4b45e7
ehunterADJ <61877009+ehunterADJ@users.noreply.github.com>
2021-04-30 12:35:25 +0200

    Update check.py

--- check.py
+++ check.py
@@ -4,5 +4,5 @@ responses = requests.get("https://google.com/")
 if responses.url == "https://www.google.com/":
     print("URL matches")
 else:
-    exit(1)
+    print("URL does not match)
 print("hello")
