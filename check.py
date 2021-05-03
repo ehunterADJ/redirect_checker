9ba5c148374dcb51a756c77f92235bd1562c335f
ehunterADJ <61877009+ehunterADJ@users.noreply.github.com>
2021-05-03 12:04:52 +0200

    Update check.py

--- check.py
+++ check.py
@@ -2,8 +2,7 @@ import requests
 
 responses = requests.get("https://google.com/")
 print(responses.url)
-if responses.url == "https://www.google1.com/":
+if responses.url == "https://www.google.com/":
     print("URL matches")
 else:
     exit(1)
-print("hello 2")
